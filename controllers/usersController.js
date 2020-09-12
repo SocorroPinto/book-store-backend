d090d1d9ab009d444556349589c6da78924612ed
Socorro Pinto <pintojuanas@johndeere.com>
2020-09-12 10:51:44 -0500

    Change to the user route

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,10 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const UserModel = require("../models").User;
+const CartModel = require("../models").Cart;
 
 // GET USERS PROFILE
 router.get("/profile/:id", async (req, res) => {
-  let user = await UserModel.findByPk(req.params.id);
+  let user = await UserModel.findByPk(req.params.id, {
+    
+    include: [ { model: CartModel,
+                  required: false,
+                  where: { Status: 'New'} }],
+            });
   res.json({ user });
 });
 
