9f071d9484d4d1911159a02e8ff7c08e079512d2
Socorro Pinto <pintojuanas@johndeere.com>
2020-09-11 23:52:25 -0500

    Books Pagination - mostrated and mostselled routes

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,11 +1,90 @@
 const express = require("express");
+const db = require("../models");
 const router = express.Router();
 
 const BookModel = require("../models").Books;
 
+let Libros = class {
+  constructor() {
+    this.books = {
+      limit: null,
+      offset: null,
+      books: null
+    }
+  }
+
+  init = (query) => {
+    if ( query.hasOwnProperty('limit') ) {
+      this.books.limit = parseInt(query.limit);
+      this.books.offset = 0;
+    }
+
+    if ( query.hasOwnProperty('offset') ) {
+      this.books.offset = parseInt(query.offset);
+      this.books.limit = this.books.limit ? this.books.limit : 6;
+    }
+  }
+};
+
 router.get("/", async (req, res) => {
-    let books = await BookModel.findAll();
-    res.json({ books });
+    const myBooks = new Libros();
+    myBooks.init(req.query);
+
+    if ( myBooks.books.limit && myBooks.books.offset !== null) {
+      myBooks.books.books = await BookModel.findAll({ 
+                              limit: myBooks.books.limit, offset: myBooks.books.offset 
+                            });
+    } else {
+      myBooks.books.books = await BookModel.findAll();      
+    }
+
+    res.json({ myBooks });
+});
+
+router.get("/mostrated", async (req, res) => {
+  const myBooks = new Libros();
+  myBooks.init(req.query);
+
+  if ( myBooks.books.limit && myBooks.books.offset !== null) {
+    myBooks.books.books = await BookModel.findAll({ 
+                            order: [['Rating', 'DESC']],
+                            limit: myBooks.books.limit, offset: myBooks.books.offset 
+                          });
+  } else {
+    myBooks.books.books = await BookModel.findAll({
+      order: [['Rating', 'DESC']],
+    });      
+  }
+
+  res.json({ myBooks });
+});
+
+router.get("/mostselled", async (req, res) => {
+  const myBooks = new Libros();
+  myBooks.init(req.query);
+
+  myQuery = 'Select "Books".* ';
+  myQuery += 'From "Books" LEFT JOIN (Select "CartDetails"."BookId", sum("CartDetails"."Quantity") vendidos ';
+  myQuery += '          From "Carts" JOIN "CartDetails" ON "Carts"."id" = "CartDetails"."CartId" ';
+  myQuery += '          Where "Status" = \'Confirmed\' ';
+  myQuery += '          Group by "CartDetails"."BookId") AS "Selled" ON "Books"."id" = "Selled"."BookId" ';
+  myQuery += 'Order by "Selled"."vendidos" ';
+
+  if ( myBooks.books.limit && myBooks.books.offset !== null) {
+    myBooks.books.books = await db.sequelize.query(`${myQuery}`, {
+                              model: BookModel,
+                              limit: myBooks.books.limit, offset: myBooks.books.offset,
+                              mapToModel: true // pass true here if you have any mapped fields
+                          });
+  } else {
+    myBooks.books.books = await db.sequelize.query(`${myQuery}`, {
+                             model: BookModel,
+                             mapToModel: true // pass true here if you have any mapped fields
+                          }
+                          );     
+  }
+
+  res.json({ myBooks });
 });
 
 router.post("/", async (req, res) => {
